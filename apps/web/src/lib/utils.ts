dd4d2e0dfc128e9782e61933aec21a2c3a7c858d
Gaz Jones <gaz@lounge.com>
2026-01-16 22:03:38 +0000

    feat(utils): add createCompoundComponent utility

--- apps/web/src/lib/utils.ts
+++ apps/web/src/lib/utils.ts
@@ -1,6 +1,13 @@
+import type { FC } from "react";
+
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-export function cn(...inputs: ClassValue[]) {
+export const cn = (...inputs: ClassValue[]) => {
   return twMerge(clsx(inputs));
-}
+};
+
+export const createCompoundComponent = <TProps, TSubcomponents>(
+  Component: FC<TProps>,
+  subComponents: TSubcomponents,
+) => Object.assign(Component, subComponents);
